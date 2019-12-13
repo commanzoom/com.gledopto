ad5b24629328174d69c5647f7cc407401227ddc9
commanzoom <zoom4eg@bk.ru>
2019-12-13 22:33:06 +0100

    Added support for GL-MC-001
    Based on GL-C-008

---
+++ gledopto_gl-mc-001/device.js
@@ -0,0 +1,9 @@
+'use strict';
+
+const Homey = require('homey');
+const ZigBeeXYLightDevice = require('homey-meshdriver').ZigBeeXYLightDevice;
+
+class GledoptoGLMC001 extends ZigBeeXYLightDevice {
+
+}
+module.exports = GledoptoGLMC001;
